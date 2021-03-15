6d7eecc90b38140b3ca77df9610d21fc647139be
Leanne Dong <jdleanne@gmail.com>
2021-03-15 01:24:33 -0400

    upload some data

--- main.cpp
+++ main.cpp
@@ -4,72 +4,15 @@
 #include <utility> // std::pair
 #include <stdexcept> // std::runtime_error
 #include <sstream> // std::stringstream
+#include "include/read_write.h"
 
-std::vector<std::pair<std::string, std::vector<int>>> read_csv(std::string filename){
-    // Reads a CSV file into a vector of <string, vector<int>> pairs where
-    // each pair represents <column name, column values>
 
-    // Create a vector of <string, int vector> pairs to store the result
-    std::vector<std::pair<std::string, std::vector<int>>> result;
-
-    // Create an input filestream
-    std::ifstream myFile(filename);
-
-    // Make sure the file is open
-    if(!myFile/*.is_open()*/) throw std::runtime_error("Could not open file");
-
-    // Helper vars
-    std::string line, colname;
-    int val;
-
-    // Read the column names
-    if(myFile.good())
-    {
-        // Extract the first line in the file
-        std::getline(myFile, line);
-
-        // Create a stringstream from line
-        std::stringstream ss(line);
-
-        // Extract each column name
-        while(std::getline(ss, colname, ',')){
-
-            // Initialize and add <colname, int vector> pairs to result
-            result.push_back({colname, std::vector<int> {}});
-        }
-    }
-
-    // Read data, line by line
-    while(std::getline(myFile, line))
-    {
-        // Create a stringstream of the current line
-        std::stringstream ss(line);
-
-        // Keep track of the current column index
-        int colIdx = 0;
-
-        // Extract each integer
-        while(ss >> val){
-
-            // Add the current integer to the 'colIdx' column's values vector
-            result.at(colIdx).second.push_back(val);
-
-            // If the next token is a comma, ignore it and move on
-            if(ss.peek() == ',') ss.ignore();
-
-            // Increment the column index
-            colIdx++;
-        }
-    }
-
-    // Close file, for windows user
-   // myFile.close();
-
-    return result;
-}
 
 int main() {
     // Read three_cols.csv and ones.csv
-    std::vector<std::pair<std::string, std::vector<int>>> three_cols = read_csv("Heatin_Cooling_data.csv");
+    std::vector<std::pair<std::string, std::vector<int>>> mul_cols = read_csv("Heating_Cooling_data.csv");
+
+    // Write to another file to check that this was successful
+    write_csv("Heating_Cooling_data_copy.csv", mul_cols);
     return 0;
 }
