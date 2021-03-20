354256425fcd8f0555bb6934a20f6946bbf7d181
Leanne Dong <jdleanne@gmail.com>
2021-03-20 14:19:56 -0400

    forgot to add some codes

---
+++ HydraulicLib/include/HydraulicLib/NetworkSolve.h
@@ -0,0 +1,8 @@
+/*!
+ * Created by leanne on 3/20/21.
+ */
+
+#ifndef SIMPLEHYDRAULICNETWORK_NetworkSolve_H
+#define SIMPLEHYDRAULICNETWORK_NetworkSolve_H
+void NetworkSolve();
+#endif //SIMPLEHYDRAULICNETWORK_NetworkSolve_H
