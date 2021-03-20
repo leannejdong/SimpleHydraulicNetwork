f3054de1a8105d51484075371f84724496bf56b1
Leanne Dong <jdleanne@gmail.com>
2021-03-20 14:03:58 -0400

    added some comments

--- HydraulicLib/src/main.cpp
+++ HydraulicLib/src/main.cpp
@@ -1,5 +1,10 @@
 /*!
- * Created by leanne on 3/20/21.
+ * by including the doctest header with the special define,
+ * it generates the main() function for the doctest executable.
+ * This is connected through the src/CMakeLists.txt that compile this
+ * file and the read_write_test.cpp into one binary executable
+ * This main.cpp also allows us separate .cpp files for separate files.
+ * It makes sense to give the 'main' its own .cpp file
  */
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
